d5562d27d31f5bb7666d8b6bed2c64bb733335ab
misseven0 <misseven@protonmail.com>
2023-08-19 02:40:37 +0800

    init 1.30

---
+++ src/main/java/com/google/gwtjsonrpc/client/impl/ser/JavaSqlTimestamp_JsonSerializer.java
@@ -0,0 +1,82 @@
+// Copyright 2008 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gwtjsonrpc.client.impl.ser;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwtjsonrpc.client.impl.JsonSerializer;
+import com.google.gwtjsonrpc.client.impl.ResultDeserializer;
+import com.google.gwtjsonrpc.common.JavaSqlTimestampHelper;
+import java.sql.Timestamp;
+
+/** Default serialization for a {@link java.sql.Timestamp}. */
+public final class JavaSqlTimestamp_JsonSerializer extends JsonSerializer<java.sql.Timestamp>
+    implements ResultDeserializer<java.sql.Timestamp> {
+  public static final JavaSqlTimestamp_JsonSerializer INSTANCE =
+      new JavaSqlTimestamp_JsonSerializer();
+
+  @Override
+  public java.sql.Timestamp fromJson(final Object o) {
+    if (o != null) {
+      return parseTimestamp((String) o);
+    }
+    return null;
+  }
+
+  @Override
+  public void printJson(final StringBuilder sb, final java.sql.Timestamp o) {
+    sb.append('"');
+    sb.append(toString(o.getTime()));
+    sb.append('"');
+  }
+
+  protected static String padTwo(final int v) {
+    if (v < 10) {
+      return "0" + v;
+    }
+    return String.valueOf(v);
+  }
+
+  protected static String padThree(final int v) {
+    if (v < 10) {
+      return "00" + v;
+    } else if (v < 100) {
+      return "0" + v;
+    } else {
+      return String.valueOf(v);
+    }
+  }
+
+  private static native String toString(double utcMilli) /*-{
+    var d = new Date(utcMilli);
+    var p2 = @com.google.gwtjsonrpc.client.impl.ser.JavaSqlTimestamp_JsonSerializer::padTwo(I);
+    var p3 = @com.google.gwtjsonrpc.client.impl.ser.JavaSqlTimestamp_JsonSerializer::padThree(I);
+    return d.getUTCFullYear() + "-" +
+    p2(1 + d.getUTCMonth()) + "-" +
+    p2(d.getUTCDate())+ " " +
+    p2(d.getUTCHours()) + ":" +
+    p2(d.getUTCMinutes()) + ":" +
+    p2(d.getUTCSeconds()) + "." +
+    p3(d.getUTCMilliseconds()) + "000000";
+  }-*/;
+
+  public static java.sql.Timestamp parseTimestamp(final String s) {
+    return JavaSqlTimestampHelper.parseTimestamp(s);
+  }
+
+  @Override
+  public Timestamp fromResult(JavaScriptObject responseObject) {
+    return fromJson(PrimitiveResultDeserializers.stringResult(responseObject));
+  }
+}
---
+++ src/test/java/com/google/gwtjsonrpc/common/JavaSqlTimestampHelperTest.java
@@ -0,0 +1,100 @@
+// Copyright (C) 2014 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gwtjsonrpc.common;
+
+import static com.google.gwtjsonrpc.common.JavaSqlTimestampHelper.hasTime;
+import static com.google.gwtjsonrpc.common.JavaSqlTimestampHelper.hasTimeZone;
+import static com.google.gwtjsonrpc.common.JavaSqlTimestampHelper.parseTimestamp;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JavaSqlTimestampHelperTest {
+  private SimpleDateFormat format;
+  private TimeZone systemTimeZone;
+
+  @Before
+  public void setUp() throws Exception {
+    synchronized (TimeZone.class) {
+      systemTimeZone = TimeZone.getDefault();
+      TimeZone.setDefault(TimeZone.getTimeZone("GMT-5:00"));
+      format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS Z");
+    }
+  }
+
+  @After
+  public void resetTimeZone() {
+    TimeZone.setDefault(systemTimeZone);
+  }
+
+  @Test
+  public void parseFullTimestamp() {
+    assertEquals("2006-01-02 15:04:05.789 -0500", reformat("2006-01-02 20:04:05.789000000"));
+    assertEquals("2006-01-02 15:04:05.000 -0500", reformat("2006-01-02 20:04:05"));
+  }
+
+  @Test
+  public void parseDateOnly() {
+    assertEquals("2006-01-01 19:00:00.000 -0500", reformat("2006-01-02"));
+  }
+
+  @Test
+  public void parseTimeZone() {
+    assertEquals("2006-01-02 11:04:05.789 -0500", reformat("2006-01-02 15:04:05.789 -0100"));
+    assertEquals("2006-01-02 10:04:05.789 -0500", reformat("2006-01-02 15:04:05.789 -0000"));
+    assertEquals("2006-01-02 09:04:05.789 -0500", reformat("2006-01-02 15:04:05.789 +0100"));
+  }
+
+  @Test
+  public void parseInvalidTimestamps() {
+    assertInvalid("2006-01-02-15:04:05.789000000");
+    assertInvalid("2006-01-02T15:04:05.789000000");
+    assertInvalid("15:04:05");
+    assertInvalid("15:04:05.999000000");
+  }
+
+  @Test
+  public void testHasTimeZone() {
+    assertTrue(hasTimeZone("2006-01-02 15:04:05.789 -0500"));
+    assertFalse(hasTimeZone("2006-01-02 15:04:05.789"));
+  }
+
+  @Test
+  public void testHasTime() {
+    assertTrue(hasTime("2006-01-02 15:04:05.789 -0500"));
+    assertTrue(hasTime("2006-01-02 15:04:05.789"));
+    assertFalse(hasTime("2006-01-02"));
+  }
+
+  private static void assertInvalid(String input) {
+    try {
+      parseTimestamp(input);
+      fail("Expected IllegalArgumentException for: " + input);
+    } catch (IllegalArgumentException e) {
+      // Expected;
+    }
+  }
+
+  private String reformat(String input) {
+    return format.format(parseTimestamp(input));
+  }
+}
